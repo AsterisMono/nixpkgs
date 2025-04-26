81f45e1691a26551a2678344edae38d44c164925
Chatnoir Miki <cmiki@amono.me>
2025-04-26 23:36:50 +0800

    sqlitestudio: include official plugins

--- pkgs/by-name/sq/sqlitestudio/package.nix
+++ pkgs/by-name/sq/sqlitestudio/package.nix
@@ -41,7 +41,30 @@ stdenv.mkDerivation rec {
       qtscript
     ]);
 
-  qmakeFlags = [ "./SQLiteStudio3" ];
+  hardeningDisable = [
+    "fortify"
+  ];
+
+  configurePhase = ''
+    runHook preConfigure
+    export SRC=$NIX_BUILD_TOP/${src.name}
+    mkdir $SRC/build-base && cd $SRC/build-base
+    qmake $SRC/SQLiteStudio3 \
+      "DEFINES += NO_AUTO_UPDATES" \
+      "DEFINES += PLUGINS_DIR=${placeholder "out"}/lib/sqlitestudio"
+    mkdir $SRC/build-plugins && cd $SRC/build-plugins
+    qmake $SRC/Plugins \
+      "PYTHON_VERSION = ${python3.pythonVersion}" \
+      "INCLUDEPATH += ${python3}/include/python${python3.pythonVersion}"
+    runHook postConfigure
+  '';
+
+  buildPhase = ''
+    cd $SRC/build-base
+    make -j $NIX_BUILD_CORES
+    cd $SRC/build-plugins
+    make -j $NIX_BUILD_CORES
+  '';
 
   desktopItems = [
     (makeDesktopItem {
@@ -56,9 +79,18 @@ stdenv.mkDerivation rec {
     })
   ];
 
+  installPhase = ''
+    runHook preInstall
+    cd $SRC/build-base
+    make install INSTALL_ROOT=$out
+    cd $SRC/build-plugins
+    make install INSTALL_ROOT=$out
+    runHook postInstall
+  '';
+
   postInstall = ''
     install -Dm755 \
-      ./SQLiteStudio3/guiSQLiteStudio/img/sqlitestudio.svg \
+      $SRC/SQLiteStudio3/guiSQLiteStudio/img/sqlitestudio.svg \
       $out/share/pixmaps/sqlitestudio.svg
   '';
 
